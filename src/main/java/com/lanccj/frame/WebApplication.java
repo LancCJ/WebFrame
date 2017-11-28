e381b52b336f427e3f56cd3a15fb17834c2e1f9d
lanccj <lanccj@163.com>
2017-11-28 16:32:02 +0800

    2017年11月28日16:31:54

--- src/main/java/com/lanccj/frame/WebApplication.java
+++ src/main/java/com/lanccj/frame/WebApplication.java
@@ -1,9 +1,11 @@
 package com.lanccj.frame;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * \* @created with IntelliJ IDEA.
@@ -12,9 +14,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * \* @time: 下午2:53
  * \* @to change this template use File | Settings | File Templates.
  * \* @description:
+ *
+ * @ServletComponentScan 设置启动时spring能够扫描到我们自己编写的servlet和filter, 用于Druid监控
+ * @MapperScan 用于扫描的mapper接口
  */
 
 @SpringBootApplication
+@ServletComponentScan
+@MapperScan({"com.lanccj.frame.mapper","com.cybertech.mapper"})
 public class WebApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lanccj/frame/controller/HelloController.java
+++ src/main/java/com/lanccj/frame/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.lanccj.frame.controller;
 
+import com.lanccj.frame.dao.TestMapper;
+import com.lanccj.frame.entity.Test;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * \* @created with IntelliJ IDEA.
@@ -15,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Resource
+    private TestMapper testMapper;
+
     @RequestMapping(value = "/hello")
     public String hello() {
         return "hello,Spring Boot";
@@ -23,6 +30,15 @@ public class HelloController {
     @RequestMapping(value = "/value")
     public String value() {
         return "hello,Spring Boot value";
+    }
+
+    @RequestMapping(value = "/data")
+    public String data() {
+        Test test = new Test();
+        test.setId(1L);
+        test.setName("Test");
+        testMapper.insert(test);
+        return "hello,Spring Boot data";
     }
 
 }
